d1858d64ca407996a28f6069ce5492f76de30887
Denny da Vjncj <daVjncjCode@gmail.com>
2020-12-16 13:07:21 -0500

    tentative updates

--- develop/models/Product.js
+++ develop/models/Product.js
@@ -7,8 +7,7 @@ const sequelize = require('../config/connection');
 class Product extends Model {}
 
 // set up fields and rules for Product model
-Product.init(
-  {
+Product.init({
     id:{
       type:DataTypes.INTEGER,
       allowNull:false,
@@ -32,6 +31,13 @@ Product.init(
       defaultValue:10,
       validate:{
         isNumeric:true
+      }
+    },
+    categoryId:{
+      type:DataTypes.INTEGER,
+      references:{
+        model:'category',
+        key:'id'
       }
     }
   },
