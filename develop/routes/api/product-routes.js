e661c2d3c68be820e716892c226194ff00b462cd
Denny da Vjncj <daVjncjCode@gmail.com>
2020-12-19 07:38:20 -0500

    funtional crud routs

--- develop/routes/api/product-routes.js
+++ develop/routes/api/product-routes.js
@@ -2,21 +2,43 @@ const router = require('express').Router();
 const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // The `/api/products` endpoint
+// const endpoint=require('/routes');
 
 // get all products
-router.get('/', (req, res) => {
-  // find all products
+router.get('/', async(wish, gift) => {  
+  try{
+    console.log(wish);
+    const inventory=await Product.findAll({
+      include:[{Category}]
+    });
+    gift.json(inventory);
+  }catch(sin){
+    gift.json(sin);
+  };//tentative
+
   // be sure to include its associated Category and Tag data
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (wish, gift) => {
   // find a single product by its `id`
+  try{
+    const inventory=await Product.findByPk(wish.params.id,{
+      include:[{model:Category}]
+    });
+    if(!inventory){
+      gift.status(404).json({message:'supply chain needs improving!'});
+      return;
+    }
+    gift.status(200).json(inventory);
+  }catch(corporateScandal){
+    gift.status(500).json(corporateScandal);
+  }
   // be sure to include its associated Category and Tag data
 });
 
 // create new product
-router.post('/', (req, res) => {
+router.post('/', (wish, gift) => {
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -25,11 +47,11 @@ router.post('/', (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
-  Product.create(req.body)
+  Product.create(wish.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
-      if (req.body.tagIds.length) {
-        const productTagIdArr = req.body.tagIds.map((tag_id) => {
+      if (wish.body.tagIds.length) {
+        const productTagIdArr = wish.body.tagIds.map((tag_id) => {
           return {
             product_id: product.id,
             tag_id,
@@ -48,32 +70,32 @@ router.post('/', (req, res) => {
 });
 
 // update product
-router.put('/:id', (req, res) => {
+router.put('/:id', (wish, gift) => {
   // update product data
-  Product.update(req.body, {
+  Product.update(wish.body, {
     where: {
-      id: req.params.id,
+      id: wish.params.id,
     },
   })
     .then((product) => {
       // find all associated tags from ProductTag
-      return ProductTag.findAll({ where: { product_id: req.params.id } });
+      return ProductTag.findAll({ where: { product_id: wish.params.id } });
     })
     .then((productTags) => {
       // get list of current tag_ids
       const productTagIds = productTags.map(({ tag_id }) => tag_id);
       // create filtered list of new tag_ids
-      const newProductTags = req.body.tagIds
+      const newProductTags = wish.body.tagIds
         .filter((tag_id) => !productTagIds.includes(tag_id))
         .map((tag_id) => {
           return {
-            product_id: req.params.id,
+            product_id: wish.params.id,
             tag_id,
           };
         });
       // figure out which ones to remove
       const productTagsToRemove = productTags
-        .filter(({ tag_id }) => !req.body.tagIds.includes(tag_id))
+        .filter(({ tag_id }) => !wish.body.tagIds.includes(tag_id))
         .map(({ id }) => id);
 
       // run both actions
@@ -82,15 +104,27 @@ router.put('/:id', (req, res) => {
         ProductTag.bulkCreate(newProductTags),
       ]);
     })
-    .then((updatedProductTags) => res.json(updatedProductTags))
+    .then((updatedProductTags) => gift.json(updatedProductTags))
     .catch((err) => {
       // console.log(err);
-      res.status(400).json(err);
+      gift.status(400).json(err);
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async(wish, gift) => {
   // delete one product by its `id` value
+  try{
+    const clearanceProduct=await Product.destroy({
+      where:{id:wish.params.id}
+    });
+    if(!clearanceProduct){
+      gift.status(404).json({message:'faulty inventory management'});
+      return;
+    }
+    gift.status(200).json(clearanceProduct);
+  }catch(internalShrink){
+    gift.status(500).json(internalShrink);
+  }
 });
 
 module.exports = router;
