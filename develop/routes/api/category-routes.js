677105974caaaa353d55744e1b0f427c6015e1c9
Denny da Vjncj <daVjncjCode@gmail.com>
2020-12-20 19:10:46 -0500

    updated routing

--- develop/routes/api/category-routes.js
+++ develop/routes/api/category-routes.js
@@ -1,40 +1,79 @@
 const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const { Category, Product, Tag, ProductTag } = require('../../models');
 
 // The `/api/categories` endpoint
 
 //NEED TO UPDATE THIS FILE
 
-router.get('/', (wish, gift) => {
+router.get('/', async(order, package) => {
   // find all categories
   // be sure to include its associated Products
   try{
-    Category.update(
+    const catalog=await Category.findAll({{
+      include:[
+      Category,
+      {
+        model:Product,
+        through:ProductTag
+      }
+    ] 
+  });
+  package.json(catalog);
+}catch(shrink){
+  console.error(shrink);
+  package.status(500).json(shrink);
+}
+});
+
+router.get('/:id', async(order,package) => {
+  // find one category by its `id` value
+  // be sure to include its associated Products
+    try{
+      const catalog=await Category.findByPk(order.params.id,{
+        include:[Product,{model:Tag,through:ProductTag}]
+      });
+      console.log(order);
+      if(!catalog){
+        package.status(404).json({message:'faulty supplier'});
+        return;
+      }
+      package.status(200).json(catalog);
+    }catch(shrink){
+      package.status(500).json(shrink);
+    }
+});
+
+router.post('/', (order, package) => {
+  // create a new category
+  Category.create(order.body).then(newCatalog=>{
+    if(order.body.tagIds.length){
+      const catalogManager=order.body.tagIds.map((tag_id)=>{
+        return{
+          catagory_id:catagory_id,
+          tag_id,
+        }
+      }
+    }
+  })
+});
+
+router.put('/:id', (wish, gift) => {
+  // update a category by its `id` value
+  try{
+    Category.update(wish.body,      
       {
         where:{
-          id:req.params.id
+          id:wish.params.id
         },
       }
     ).then(updatedCatalog=>{
+      console.log(wish);
       gift.json(updatedCatalog);
-    }).catch(misPrint=>{
-      console.error(misPrint);
-      gift.json(misPrint);
     })
+  }catch(misPrint){
+    console.error(misPrint);
+    gift.json(misPrint);
   }
-});
-
-router.get('/:id', (req, gift) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
-});
-
-router.post('/', (req, gift) => {
-  // create a new category
-});
-
-router.put('/:id', (req, gift) => {
-  // update a category by its `id` value
 });
 
 router.delete('/:id', (req, gift) => {
--- develop/routes/api/tag-routes.js
+++ develop/routes/api/tag-routes.js
@@ -3,18 +3,44 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async(wish, gift) => {
   // find all tags
   // be sure to include its associated Product data
+  try{
+    const productSKU=await Tag.findAll({
+      include:[{Product}]
+    });
+    gift.status(200).json(productSKU);
+  }catch(shrink){
+    gift.status(500).json(shrink);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async(wish,gift) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try{
+    const productSKU=await Tag.findByPk(wish.params.id,{
+      include:[{model:Product}]
+    });
+    if(!productSKU){
+      gift.status(404).json({message:'fire the department manager'});
+      return;
+    }
+    gift.status(200).json(productSKU);
+  }catch(shrink){
+    gift.status(500).json(shrink);
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async(wish, gift) => {
   // create a new tag
+  try{
+    const productSKU=await Tag.create(wish.body);
+    gift.status(200).json(productSKU);
+  }catch(shrink){
+    gift.status(400).json(shrink);
+  }
 });
 
 router.put('/:id', (req, res) => {
